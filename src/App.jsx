367e94e33bd0b1d93476561ab901c309006f8cab
chayy <chaitanyarocks.kulkarni@gmail.com>
2026-03-06 19:36:13 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import React, { useState } from 'react'
 import Navbar from './Components/Navbar'
 import NewsBoard from './Components/NewsBoard'
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   const [category, setCategory] = useState('general')
+
   return (
     <div>
       <Navbar setCategory={setCategory}/>
       <NewsBoard category={category}/>
+      <Analytics />
     </div>
   )
 }
 
 export default App
-
-
-
-
-
-
-
